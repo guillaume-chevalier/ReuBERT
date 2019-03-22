8429312aba561b444b6db6a73a55013c157c06ab
Julie Tetrault <julie.tetrault.2@gmail.com>
2019-03-22 17:02:36 -0400

    Reformat

--- test/infrastructure/persistence/test_in_memory_input_text_repository.py
+++ test/infrastructure/persistence/test_in_memory_input_text_repository.py
@@ -8,30 +8,23 @@ class TestInMemoryInputTextRepository(unittest.TestCase):
     def setUp(self):
         self.input_text_repository = InMemoryInputTextRepository()
 
-    def test__repository_containing_context_statements__when__get_all_context_statements__then__returns_all_context_statements(self):
+    def test__repository_containing_context_statements__when__get_all_context_statements__then__returns_all_context_statements(
+            self):
         some_context_statement = "some context statement"
         self.input_text_repository.add_context_statement(some_context_statement)
 
-        actual_context_statements =  self.input_text_repository.get_all_context_statements()
+        actual_context_statements = self.input_text_repository.get_all_context_statements(
+        )
 
         expected_context_statements = [some_context_statement]
         self.assertEqual(expected_context_statements, actual_context_statements)
 
-
-    def test__repository_containing_questions__when__get_all_questions__then__returns_all_questions(self):
+    def test__repository_containing_questions__when__get_all_questions__then__returns_all_questions(
+            self):
         some_question = "some context statement"
         self.input_text_repository.add_question(some_question)
 
-        actual_questions =  self.input_text_repository.get_all_questions()
+        actual_questions = self.input_text_repository.get_all_questions()
 
         expected_questions = [some_question]
         self.assertEqual(expected_questions, actual_questions)
-
-
-
-
-
-
-
-
-
