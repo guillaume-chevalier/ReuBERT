2de9e9c58c907fdb6e6ecdb2dc732c9f3c8aec06
Vaunorage <vaunorage@gmail.com>
2019-03-23 20:04:12 -0400

    Added levenstein diffrence to test anwsers accuracy Added multiple test difficulties (uncompleted)
    Signed-off-by: Vaunorage <vaunorage@gmail.com>

--- test/acceptance/acceptence_test.py
+++ test/acceptance/acceptence_test.py
@@ -1,6 +1,8 @@
 import json
 import os
+
 import pytest
+import stringdist
 
 from src.infrastructure.bert_model_wrapper import BertModelWrapper
 from src.infrastructure.trained_bert_q_a_model import TrainedBERTQuestionAnsweringModel, get_reubert_flags
@@ -10,7 +12,8 @@ bert_model = TrainedBERTQuestionAnsweringModel(flags)
 
 bert_wrapper = BertModelWrapper(bert_model)
 
-# Todo : put different levels of questions : easy , meduim, hard, impossible
+
+# Todo : put different levels of questions : easy , medium, hard, impossible
 def load_json_file_test(json_name):
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), json_name), encoding="utf8") as json_data:
         return json.load(json_data)
@@ -21,31 +24,41 @@ QATEST_FILE_2 = load_json_file_test('QA_test_2.json')
 QATEST_FILE_3 = load_json_file_test('QA_test_3.json')
 
 
-@pytest.mark.parametrize("QA_test", [QATEST_FILE_1, QATEST_FILE_2, QATEST_FILE_3])
-def test_model_answer_test_1(QA_test):
+@pytest.mark.parametrize("QA_test", [QATEST_FILE_1])
+@pytest.mark.parametrize("difficulty", ["easy"])
+def test__given__user_input__when__asking_questions_to_bert_model_wrapper__then__get_good_results(QA_test, difficulty):
     user_input = QA_test['user_inputs']
 
-    question_1 = QA_test['QA'][0]['question']
-    answers_1 = QA_test['QA'][0]['answers']
+    question_1 = QA_test['QA'][difficulty][0]['question']
+    expected_answers_1 = QA_test['QA'][difficulty][0]['answers']
 
-    question_2 = QA_test['QA'][1]['question']
-    answers_2 = QA_test['QA'][1]['answers']
+    question_2 = QA_test['QA'][difficulty][1]['question']
+    expected_answers_2 = QA_test['QA'][difficulty][1]['answers']
 
-    question_3 = QA_test['QA'][2]['question']
-    answers_3 = QA_test['QA'][2]['answers']
+    question_3 = QA_test['QA'][difficulty][2]['question']
+    expected_answers_3 = QA_test['QA'][difficulty][2]['answers']
 
-    question_4 = QA_test['QA'][3]['question']
-    answers_4 = QA_test['QA'][3]['answers']
+    question_4 = QA_test['QA'][difficulty][3]['question']
+    expected_answers_4 = QA_test['QA'][difficulty][3]['answers']
 
-    # Model.load(user_input)
+    response_1 = bert_wrapper.transform(bert_wrapper.question_schema(user_input, question_1))
+    response_2 = bert_wrapper.transform(bert_wrapper.question_schema(user_input, question_2))
+    response_3 = bert_wrapper.transform(bert_wrapper.question_schema(user_input, question_3))
+    response_4 = bert_wrapper.transform(bert_wrapper.question_schema(user_input, question_4))
 
-    # response_1 = Model.answer(question_1)
-    # response_2 = Model.answer(question_2)
-    # response_3 = Model.answer(question_3)
-    # response_4 = Model.answer(question_4)
+    verify_answers(response_1, expected_answers_1)
+    verify_answers(response_2, expected_answers_2)
+    verify_answers(response_3, expected_answers_3)
+    verify_answers(response_4, expected_answers_4)
 
-    # assert response_1 in answers_1
-    # assert response_2 in answers_2
-    # assert response_3 in answers_3
-    # assert response_4 in answers_4
-    assert True
+
+def verify_answers(bert_responses, expected_responses):
+    right_answer = None
+    acceptable_levenshtein_threshold = 0.5
+
+    for bert_res in bert_responses:
+        for expected_res in expected_responses:
+            if stringdist.levenshtein_norm(bert_res[1], expected_res) < acceptable_levenshtein_threshold:
+                right_answer = bert_res
+
+    assert right_answer is not None
