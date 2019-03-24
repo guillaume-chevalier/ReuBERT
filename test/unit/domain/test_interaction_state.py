3e82c7f3176f14af6b47a2562e8b9cbcf1392ef3
Vaunorage <vaunorage@gmail.com>
2019-03-24 18:44:51 -0400

    - Trying to correct travis
    Signed-off-by: Vaunorage <vaunorage@gmail.com>

--- test/unit/domain/test_interaction_state.py
+++ test/unit/domain/test_interaction_state.py
@@ -1,9 +1,15 @@
 import pytest
+import os
 
 from src.domain.interaction_state import InteractionState
 
+print("TEST")
+print(os.getcwd())
+
 
 def test_can_create_interaction_state():
+    print("TEST")
+    print(os.getcwd())
     # Note: the test method name must start by "test_".
     _is = InteractionState()
     assert isinstance(_is, InteractionState)
