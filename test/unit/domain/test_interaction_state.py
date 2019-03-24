1a86fdafb1527829e67ca967c7eac51ec3672de4
Vaunorage <vaunorage@gmail.com>
2019-03-24 18:29:52 -0400

    - Trying to correct travis
    Signed-off-by: Vaunorage <vaunorage@gmail.com>

--- test/unit/domain/test_interaction_state.py
+++ test/unit/domain/test_interaction_state.py
@@ -1,9 +1,6 @@
 import pytest
-import os
-from src.domain.interaction_state import InteractionState
 
-print("TEST")
-print(os.getcwd())
+from src.domain.interaction_state import InteractionState
 
 
 def test_can_create_interaction_state():
