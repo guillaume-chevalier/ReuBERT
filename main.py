ba05f95c282cf9d301aab301f9def9da5988c27a
guillaume-chevalier <guillaume.chevalier@neuraxio.com>
2019-04-01 23:14:12 -0400

    DIP in infra for BertNaturalAnswerPostprocessor.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from src.api.cli.robot_interaction_resource_impl import CLIRobotInteractionResourceImpl
 from src.application.interaction.interaction_service import InteractionService
 from src.domain.pipeline import Pipeline
-from src.domain.pipeline_steps.natural_answer_postprocessor_interface import NaturalAnswerPostprocessorInterface
 from src.infrastructure.bert_model_wrapper import BertModelWrapper
+from src.infrastructure.persistence.bert_natural_answer_postprocessor import BertNaturalAnswerPostprocessor
 from src.infrastructure.persistence.interaction.in_memory_input_text_repository import InMemoryInputTextRepository
 from src.infrastructure.persistence.interaction.in_memory_interaction_state import InMemoryInteractionState
 
 if __name__ == "__main__":
     # 1. Initialize Domain Service(s).
-    pipeline = Pipeline(BertModelWrapper(), NaturalAnswerPostprocessorInterface())
+    pipeline = Pipeline(BertModelWrapper(), BertNaturalAnswerPostprocessor())
     input_text_repository = InMemoryInputTextRepository()
     interaction_state = InMemoryInteractionState(pipeline)
 
---
+++ src/infrastructure/persistence/bert_natural_answer_postprocessor.py
@@ -0,0 +1,5 @@
+from src.domain.pipeline_steps.natural_answer_postprocessor_interface import NaturalAnswerPostprocessorInterface
+
+
+class BertNaturalAnswerPostprocessor(NaturalAnswerPostprocessorInterface):
+    pass  # TODO: Taha.
