342b97c5cbe900a227cb090cea45f02bd7446e03
guillaume-chevalier <guillaume-chevalier@outlook.com>
2019-04-04 13:16:17 -0400

    Removed obsolete InMemoryInteractionState

--- src/applicative_context.py
+++ src/applicative_context.py
@@ -2,7 +2,6 @@ from src.api.cli.robot_interaction_resource_impl import CLIRobotInteractionResou
 from src.application.interaction.interaction_service import InteractionService
 from src.domain.pipeline import Pipeline
 from src.infrastructure.persistence.interaction.in_memory_input_text_repository import InMemoryInputTextRepository
-from src.infrastructure.persistence.interaction.in_memory_interaction_state import InMemoryInteractionState
 from src.infrastructure.pipeline_steps.bert_model_wrapper import BertModelWrapper
 from src.infrastructure.pipeline_steps.bert_natural_answer_postprocessor import BertNaturalAnswerPostprocessor
 
@@ -19,10 +18,9 @@ class CLIReuBERTApplicativeContext:
     def _initialize_domain_services(self):
         self.pipeline = Pipeline(BertModelWrapper(), BertNaturalAnswerPostprocessor())
         self.input_text_repository = InMemoryInputTextRepository()
-        self.interaction_state = InMemoryInteractionState(self.pipeline)
 
     def _initialize_application_services(self):
-        self.interaction_service = InteractionService(self.input_text_repository, self.interaction_state)
+        self.interaction_service = InteractionService(self.input_text_repository, self.pipeline)  # TODO: pass pipeline
         self.robot_interaction_resource = CLIRobotInteractionResourceImpl(self.interaction_service)
 
     def execute(self):
