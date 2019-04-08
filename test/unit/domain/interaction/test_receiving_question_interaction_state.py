ba81de9ee6394cd0d46fecbf728c2ab7cfca4ef3
Julie Tetrault <julie.tetrault.2@gmail.com>
2019-04-08 00:08:01 -0400

    Tests for ReceivingQuestionInteractionState

--- test/unit/domain/interaction/test_receiving_question_interaction_state.py
+++ test/unit/domain/interaction/test_receiving_question_interaction_state.py
@@ -8,12 +8,22 @@ from src.domain.interaction.receiving_question_interaction_state import Receivin
 class TestReceivingQuestionInteractionState(unittest.TestCase):
 
     _SOME_INPUT_TEXT = "some question"
+    _SOME_RESPONSE = "some response"
 
     def setUp(self):
         self.receivingQuestionInteractionState = ReceivingQuestionInteractionState()
         self.input_text_processor_mock = MagicMock(spec=InputTextProcessor)
 
     def test__when__processing_input_text__then__input_text_processor_processes_given_question(self):
-        self.receivingQuestionInteractionState.process_input_text(self._SOME_INPUT_TEXT,  self.input_text_processor_mock)
+        self.receivingQuestionInteractionState.process_input_text(self._SOME_INPUT_TEXT, self.input_text_processor_mock)
 
         self.input_text_processor_mock.process_question.assert_called_once_with(self._SOME_INPUT_TEXT)
+
+    def test__when__processing_input_text__then__returns_appropriate_response(self):
+        expected_response = self._SOME_RESPONSE
+        self.input_text_processor_mock.process_question.return_value = expected_response
+
+        actual_response = self.receivingQuestionInteractionState.process_input_text(self._SOME_INPUT_TEXT,
+                                                                                    self.input_text_processor_mock)
+
+        self.assertEqual(expected_response, actual_response)
