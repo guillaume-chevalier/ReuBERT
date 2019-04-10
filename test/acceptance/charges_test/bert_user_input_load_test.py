f52f8b408b379e5ada17be0bb4d4ec307eab4901
Vaunorage <vaunorage@gmail.com>
2019-04-10 16:55:39 -0400

    Added tests to see how bert peroformed on different text length
    Signed-off-by: Vaunorage <vaunorage@gmail.com>

--- test/acceptance/charges_test/bert_user_input_load_test.py
+++ test/acceptance/charges_test/bert_user_input_load_test.py
@@ -1,46 +1,44 @@
-import json
 import os
+import json
+import time
 
-from src.util.response_evaluator import ResponseEvaluator
 from src.infrastructure.pipeline_steps.bert_model_wrapper import BertModelWrapper
+from src.infrastructure.pipeline_steps.bert_natural_answer_postprocessor import BertNaturalAnswerPostprocessor
 
-
-# Todo : put different levels of questions : easy , medium, hard, impossible
 def load_json_file_test(json_name):
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), json_name), encoding="utf8") as json_data:
         return json.load(json_data)
 
 
-SQUAD_TEST_FILE = load_json_file_test('../../../thales-bert-gcp-bucket/squad_dir/squad_questions_beautified.json')
+num_words = ['50', '100', '150', '200', '250', '300', '350', '400', '450', '500', '550', '600', '650', '700',
+             '750', '800', '850', '900', '950', '1000']
+def run_load_test():
+    mock_test_file = load_json_file_test('load_tests_file_mock.json')
+    bert_wrapper = BertModelWrapper()
+    best_post_processor = BertNaturalAnswerPostprocessor()
 
-#Todo: still uncomplete maybe useful for the report or the presentation to see how bert reacts to different input text length
+    for num_word in num_words:
+        start_time = time.time()
+        print('----------------------------')
+        print('load test with ', num_word, ' words')
+        print('started : ', start_time)
+        input = mock_test_file[num_word]['input']
+        question = mock_test_file[num_word]['question']
+
+        print('question : ', question)
+
+        output = bert_wrapper.transform_one(([input], question))
+
+        transformed_answer = best_post_processor.transform_one(output)
+
+        print('answer : ', transformed_answer)
+
+        finish_time = time.time()
+        taken_time = finish_time - start_time
+
+        print('finished : ', finish_time)
+        print('it took in sec : ', taken_time)
 
 
-def run_bert_user_input_load_test():
-    for elem in SQUAD_TEST_FILE:
-        sections = elem["qa"]
-        user_input, questions = get_proportion(sections,)
-        bert_wrapper = BertModelWrapper()
-        response = bert_wrapper.transform((user_input, question))
+run_load_test()
 
-        verify_answers(response, expected_answers)
-
-
-def get_proportion(sections, portion):
-    portion = int(portion * len(sections))
-    text = [sections[i]["qa"]["subtext"] for i in range(portion)]
-    questions = [sections[i]["qa"]["qas"] for i in range(portion)]
-    flat_question_list = [item for sublist in questions for item in sublist]
-    return text, flat_question_list
-
-
-def verify_answers(bert_responses, expected_responses):
-    response_evaluator = ResponseEvaluator()
-    right_answer = None
-
-    for bert_res in bert_responses:
-        for expected_res in expected_responses:
-            if response_evaluator.is_response_close_enough_using_leveinstein(bert_res[1], expected_res):
-                right_answer = bert_res
-
-    assert right_answer is not None
