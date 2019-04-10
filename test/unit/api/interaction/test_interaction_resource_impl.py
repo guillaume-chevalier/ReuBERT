2f452f9af0b4716654b79978926df70ee0c4ef19
Julie Tetrault <julie.tetrault.2@gmail.com>
2019-04-10 00:07:05 -0400

    Testing that animation thread is started and then joined

--- test/unit/api/interaction/test_interaction_resource_impl.py
+++ test/unit/api/interaction/test_interaction_resource_impl.py
@@ -24,9 +24,21 @@ class TestInteractionResourceImpl(unittest.TestCase):
         )
         self.interaction_service_mock.process_input_text.side_effect = self._SOME_REUBERT_OUTPUTS
 
+    @patch("src.api.response.response_type.initiating_interaction_response.print")
+    @patch("src.api.interaction.waiting_animation_thread.Thread.join")
+    @patch("src.api.interaction.waiting_animation_thread.Thread.start")
+    def test__when__executing__then__waiting_animation_thread_is_started_then_terminated_each_time_user_input_is_collected(
+        self, start_thread_mock, join_thread_mock, initiating_interaction_response_mock
+    ):
+        with patch('builtins.input', side_effect=self._SOME_USER_INPUTS):
+            self.interaction_resource_impl.execute()
+            self.assertEqual(start_thread_mock.call_count, len(self._SOME_USER_INPUTS))
+            self.assertEqual(join_thread_mock.call_count, len(self._SOME_USER_INPUTS))
+
+    @patch("src.api.response.response_type.initiating_interaction_response.print")
     @patch("src.api.interaction.waiting_animation_thread.Thread")
     def test__when__executing__then__interaction_service_processes_all_user_inputs_until_exit_phase_is_reached(
-        self, thread_mock
+        self, thread_mock, initiating_interaction_response_mock
     ):
         with patch('builtins.input', side_effect=self._SOME_USER_INPUTS):
             interaction_service_calls_with_user_inputs = [
@@ -39,9 +51,10 @@ class TestInteractionResourceImpl(unittest.TestCase):
                 interaction_service_calls_with_user_inputs, any_order=False
             )
 
+    @patch("src.api.response.response_type.initiating_interaction_response.print")
     @patch("src.api.interaction.waiting_animation_thread.Thread")
     def test__when__executing__then__response_factory_wraps_all_reuBERT_outputs_in_response_and_prints_them_until_exit_phase_is_reached(
-        self, thread_mock
+        self, thread_mock, initiating_interaction_response_mock
     ):
         with patch('builtins.input', side_effect=self._SOME_USER_INPUTS):
             response_factory_calls_with_reuBERT_outputs = [
